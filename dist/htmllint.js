85d5334aab8dbb4ef02f47cf54c94d34e6f920d0
alexlamsl <alexlamsl@gmail.com>
2016-05-01 22:05:14 +0800

    Version 1.1.0

--- dist/htmllint.js
+++ dist/htmllint.js
@@ -1,5 +1,5 @@
 /*!
- * HTMLLint v1.0.0 (http://kangax.github.io/html-lint/)
+ * HTMLLint v1.1.0 (http://kangax.github.io/html-lint/)
  * Copyright 2010-2016 Juriy "kangax" Zaytsev
  * Licensed under the MIT license
  */
